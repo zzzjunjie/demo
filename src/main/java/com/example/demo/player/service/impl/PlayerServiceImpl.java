4ca9bf1f455100985b779793475ae84b9d5a4ed0
zhoujunjie <zhoujunjie@37.com>
2022-04-29 15:16:30 +0800

    feat-完成玩家基础信息接口获取功能实现

---
+++ src/main/java/com/example/demo/player/service/impl/PlayerServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.player.service.impl;
+
+
+import com.example.demo.player.entity.Player;
+import com.example.demo.player.service.IPlayerService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 玩家操作实现
+ */
+@Service
+public class PlayerServiceImpl implements IPlayerService {
+
+	/**
+	 * 根据玩家ID获取玩家信息
+	 *
+	 * @param id 玩家ID
+	 * @return 玩家信息
+	 */
+	@Override
+	public Player getPlayer(int id) {
+		return new Player(id, "hello world");
+	}
+
+}
